b8c0bcb5e4007d16d1423c9972564d83376df92c
vklovo1 <56517075+vklovo1@users.noreply.github.com>
2020-04-27 22:49:53 +0200

    Fixed two functions related to creating symmetric generators

--- symm_gen.m
+++ symm_gen.m
@@ -1,6 +1,5 @@
 function E_g = symm_gen(E_ll, phi_a)
-    disp(phi_a)
     E_ll = E_ll / sqrt(3);
-    a = exp(1i * ((2/3) * pi + phi_a));
-    E_g = [E_ll*exp(1i*phi_a); E_ll*a*a; E_ll*a];
+    a = exp(1i * (2/3) * pi);
+    E_g = [E_ll * exp(1i*phi_a); E_ll * a * a * exp(1i*phi_a); E_ll*a * exp(1i*phi_a)];
 end
