18dbb3aa65b510bf3bf40f17e138fea4ed433c5d
vklovo1 <56517075+vklovo1@users.noreply.github.com>
2020-04-30 00:30:43 +0200

    Update line_pi_ekv.m

--- line_pi_ekv.m
+++ line_pi_ekv.m
@@ -1,5 +1,5 @@
 function Y = line_pi_ekv(z_s0, y_sh0, z_s1, y_sh1)
     diag = line_pi_ekv_diag(z_s0, y_sh0, z_s1, y_sh1);
-    off_diag = pi_ekv_off_diag(z_s0, z_s1);
+    off_diag = line_pi_ekv_off_diag(z_s0, z_s1);
     Y = [diag off_diag; off_diag diag];
 end
